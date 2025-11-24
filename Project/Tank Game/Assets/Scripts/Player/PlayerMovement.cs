f86895c0115ce4779b5de5811b764f85cbfd198d
AlexHardy4 <alex.hardy@mail.bcu.ac.uk>
2025-11-24 12:37:28 +0000

    Tank Movement

--- Project/Tank Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Project/Tank Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D Rigidbody2D;
+
+    [SerializeField]
+    [Header("Movement Settings")]
+    InputAction moveAction;
     public float speed;
     public float rotationSpeed;
+
+    [SerializeField]
+    [Header("Audio")]
+    public AudioSource idle;
+    public AudioClip moving;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+
+    public void Move()
     {
         if (Input.GetKey(KeyCode.W))
         {
